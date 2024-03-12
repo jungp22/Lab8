9f004a5ec9c9f0a7e7dd4cc7d769bc4a622b5c48
Jung-Hwan Park <jungpark.2002@gmail.com>
2024-03-11 21:08:21 -0600

    Step B

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -4,42 +4,42 @@ const BASE_URL = "https://dummyjson.com/products";
 
 // Fetches a list of all products
 export const fetchProducts = async () => {
-  try {
-    const response = await axios.get(BASE_URL);
-    return response.data;
-  } catch (error) {
-    throw error; // Rethrow to allow caller to handle
-  }
+	try {
+		const response = await axios.get(BASE_URL);
+		return response.data;
+	} catch (error) {
+		throw error; // Rethrow to allow caller to handle
+	}
 };
 
 // Fetches details for a single product by its ID
 export const getProductDetails = async (id) => {
-  // implement it using similar logic as fetchProducts function
-  try {
-    const response = await axios.get(`${BASE_URL}/${id}`);
-    return response.data;
-  } catch (error) {
-    throw error; // Rethrow to allow caller to handle
-  }
+	// implement it using similar logic as fetchProducts function
+	try {
+		const response = await axios.get(`${BASE_URL}/${id}`);
+		return response.data;
+	} catch (error) {
+		throw error; // Rethrow to allow caller to handle
+	}
 };
 
 // Deletes a product by its ID
 export const removeProduct = async (id) => {
-  try {
-    const response = await axios.delete(`${BASE_URL}/${id}`);
-    return response.data;
-  } catch (error) {
-    throw error; // Rethrow to allow caller to handle
-  }
+	try {
+		const response = await axios.delete(`${BASE_URL}/${id}`);
+		return response.data;
+	} catch (error) {
+		throw error; // Rethrow to allow caller to handle
+	}
 };
 
 // Adds a new product to the database
 export const addProduct = (product) => {
-  return axios.post(`${BASE_URL}/add`, JSON.stringify(product));
+	return axios.post(`${BASE_URL}/add`, JSON.stringify(product));
 };
 
 // Edits an existing product by ID
 export const editProduct = (id, product) => {
-  // implement it using similar logic as addProduct function but use axios.put method
-  return axios.put(`${BASE_URL}/add`, JSON.stringify(product));
+	// implement it using similar logic as addProduct function but use axios.put method
+	return axios.put(`${BASE_URL}/${id}`, JSON.stringify(product));
 };
